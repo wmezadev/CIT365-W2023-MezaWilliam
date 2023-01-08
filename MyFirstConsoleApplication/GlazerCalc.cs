d36ff5ba508d9e8278500b9d59cf144354f66d47
William Meza <williameduardomeza@gmail.com>
2023-01-08 00:29:00 -0500

    week1: Create class Person, prompt input and output text

---
+++ MyFirstConsoleApplication/GlazerCalc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
+{
+    internal class GlazerCalc
+    {
+        public static void RunExample()
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.WriteLine("Type width:");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Type height:");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine($"The length of the wood is {woodLength} feet");
+            Console.WriteLine($"The area of the glass is {glassArea} square metres");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text;  
 
 namespace MyFirstConsoleApplication
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            GetUserNameAndLocation();
+            ChristmasCountdown();
+            PauseApplicationPrompt();
+            GlazerCalc.RunExample();
+        }
+
+        private static void GetUserNameAndLocation()
+        {
+            Console.WriteLine("Type name");
+            string personName = Console.ReadLine();
+
+            Console.WriteLine("Type location");
+            string personLocation = Console.ReadLine();
+
+            var person = new Person
+            {
+                Name = personName,
+                Location = personLocation,
+            };
+
+            Console.WriteLine($"My name is {person.Name}, I am from {person.Location}.");
+        }
+
+        public static void PauseApplicationPrompt()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        public static void ChristmasCountdown()
+        {
+            DateTime currentDate = DateTime.Now;
+            string dateString = currentDate.ToString("MM/dd/yyyy");
+            Console.WriteLine($"Current date is {dateString}");
+            DateTime christmas = new DateTime(currentDate.Year, 12, 25);
+            TimeSpan difference = christmas - currentDate;
+            int daysUntilChristmas = difference.Days;
+            Console.WriteLine($"There are {daysUntilChristmas} days until Christmas.");
         }
     }
 }
