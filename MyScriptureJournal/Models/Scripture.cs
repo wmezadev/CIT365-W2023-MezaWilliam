da16aebb0140a970d86622c67c6c66c8306fea36
wmezadev <williameduardomeza@gmail.com>
2023-02-19 00:28:56 -0500

    week7: Copy from Movie app

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Book { get; set; } = string.Empty;
+        public int Chapter { get; set; }
+        public string Verse { get; set; } = string.Empty;
+        public string Note { get; set; } = string.Empty;
+
+        [Display(Name = "Entry Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; } = DateTime.Today;
+    }
+}
