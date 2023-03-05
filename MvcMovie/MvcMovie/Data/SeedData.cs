06e3bb316b61d50c6dc03d4ac2e8b900bf5db445
wmezadev <williameduardomeza@gmail.com>
2023-03-05 01:29:34 -0500

    week9: MVC movie app

---
+++ MvcMovie/MvcMovie/Data/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "The RM",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Comedy",
+                    Rating = "R",
+                    Price = 8
+                },
+                new Movie
+                {
+                    Title = "Other Side of Heaven",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Drama",
+                    Rating = "R",
+                    Price = 10
+                },
+                new Movie
+                {
+                    Title = "Meet the Mormons",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Documental",
+                    Rating = "R",
+                    Price = 9
+                },
+                new Movie
+                {
+                    Title = "Charly",
+                    ReleaseDate = DateTime.Parse("2002-4-15"),
+                    Genre = "Romantic",
+                    Rating = "R",
+                    Price = 5
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
